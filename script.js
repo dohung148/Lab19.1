1dc1ce0f0c99ef3d1a3dcaa7f1cee9ffca4d0d92
dohung148 <132824107+dohung148@users.noreply.github.com>
2023-07-08 14:19:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.message').textContent = 'Bat dau!!!';
   document.querySelector('.score').textContent = score;
-  document.querySelector('number').textContent = '?';
+  document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
