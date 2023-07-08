619593af3eebb4d05cec82afba438c02ed79d58a
dohung148 <132824107+dohung148@users.noreply.github.com>
2023-07-08 11:55:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  //khi chua nhap so
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+    // khi nguoi choi thang
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Chinh xac!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //khi so nhap qua lon
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Lon hon so bi mat!!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Ban da thua';
+      document.querySelector('.score').textContent = 0;
+    }
+    //khi so nhap qua be
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Be hon so bi mat!!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Ban da thua';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
